612c734312a5b640f77f4bf5583721c4e67318ad
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-06-02 03:24:29 -0300

    FocusNFeEntity

---
+++ src/main/java/org/frekele/fiscal/focus/nfe/client/core/FocusNFeEntity.java
@@ -0,0 +1,10 @@
+package org.frekele.fiscal.focus.nfe.client.core;
+
+import java.io.Serializable;
+
+/**
+ * @author frekele - Leandro Kersting de Freitas
+ */
+public interface FocusNFeEntity extends Serializable {
+
+}
