93ae939b389ba55b6acccafef57d9f32d7edf95e
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-06-02 03:23:10 -0300

    OffsetDateTimeJsonDeserialize

---
+++ src/main/java/org/frekele/fiscal/focus/nfe/client/converter/deserialize/OffsetDateTimeJsonDeserialize.java
@@ -0,0 +1,31 @@
+package org.frekele.fiscal.focus.nfe.client.converter.deserialize;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+
+import java.io.IOException;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * @author frekele - Leandro Kersting de Freitas
+ */
+public class OffsetDateTimeJsonDeserialize extends StdDeserializer<OffsetDateTime> {
+
+    protected OffsetDateTimeJsonDeserialize() {
+        super(OffsetDateTime.class);
+    }
+
+    @Override
+    public OffsetDateTime deserialize(JsonParser jsonParser, DeserializationContext ctxt) throws IOException, JsonProcessingException {
+        if (jsonParser != null) {
+            String value = jsonParser.readValueAs(String.class);
+            if (value != null) {
+                return OffsetDateTime.parse(value, DateTimeFormatter.ISO_OFFSET_DATE_TIME);
+            }
+        }
+        return null;
+    }
+}
