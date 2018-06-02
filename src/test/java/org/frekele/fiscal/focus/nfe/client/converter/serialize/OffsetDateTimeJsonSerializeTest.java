93426d3ba676a330b5dc4bbb03844c368a7b43b2
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-06-02 03:23:27 -0300

    OffsetDateTimeJsonSerialize

---
+++ src/test/java/org/frekele/fiscal/focus/nfe/client/converter/serialize/OffsetDateTimeJsonSerializeTest.java
@@ -0,0 +1,37 @@
+package org.frekele.fiscal.focus.nfe.client.converter.serialize;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import org.frekele.fiscal.focus.nfe.client.testng.InvokedMethodListener;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import java.time.OffsetDateTime;
+
+import static org.testng.Assert.*;
+
+@Listeners(InvokedMethodListener.class)
+public class OffsetDateTimeJsonSerializeTest {
+
+    @Mock
+    JsonGenerator jsonGenerator;
+
+    @BeforeClass
+    public void beforeClass() throws Exception {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testSerialize() throws Exception {
+        OffsetDateTimeJsonSerialize jsonSerialize = new OffsetDateTimeJsonSerialize();
+        assertNotNull(jsonSerialize);
+
+        jsonSerialize.serialize(null, null, null);
+        jsonSerialize.serialize(null, jsonGenerator, null);
+        jsonSerialize.serialize(OffsetDateTime.now(), jsonGenerator, null);
+        jsonSerialize.serialize(OffsetDateTime.now().minusDays(4), jsonGenerator, null);
+        jsonSerialize.serialize(OffsetDateTime.now().plusHours(6), jsonGenerator, null);
+    }
+}
