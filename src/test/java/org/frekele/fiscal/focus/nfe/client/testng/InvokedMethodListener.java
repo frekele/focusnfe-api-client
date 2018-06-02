178ddf9cadf9765b48bb1996325e4e63d4908819
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-06-02 03:21:22 -0300

    InvokedMethodListener

---
+++ src/test/java/org/frekele/fiscal/focus/nfe/client/testng/InvokedMethodListener.java
@@ -0,0 +1,35 @@
+package org.frekele.fiscal.focus.nfe.client.testng;
+
+import org.testng.IInvokedMethod;
+import org.testng.IInvokedMethodListener;
+import org.testng.ITestResult;
+
+/**
+ * @author frekele - Leandro Kersting de Freitas
+ */
+public class InvokedMethodListener implements IInvokedMethodListener {
+
+    @Override
+    public void beforeInvocation(IInvokedMethod method, ITestResult testResult) {
+        System.out.println(this.getTestClassAndMethodName(method) + " Start method --->");
+    }
+
+    @Override
+    public void afterInvocation(IInvokedMethod method, ITestResult testResult) {
+        long duration = testResult.getEndMillis() - testResult.getStartMillis();
+        System.out.println(this.getTestClassAndMethodName(method) + " Duration: [ " + duration + "ms ];");
+        System.out.println(this.getTestClassAndMethodName(method) + " <--- End method.\n");
+    }
+
+    private String getTestClassAndMethodName(IInvokedMethod method) {
+        return this.getTestClassName(method) + "[" + this.getMethodName(method) + "]";
+    }
+
+    private String getTestClassName(IInvokedMethod method) {
+        return method.getTestResult().getInstanceName();
+    }
+
+    private String getMethodName(IInvokedMethod method) {
+        return method.getTestMethod().getMethodName();
+    }
+}
