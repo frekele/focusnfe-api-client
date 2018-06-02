063b8326fb71f17307d0d8f2580615be223d5cbc
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-06-02 06:57:47 -0300

    FocusNFeV2Repository

---
+++ src/main/java/org/frekele/fiscal/focus/nfe/client/repository/nfe/FocusNFeV2Repository.java
@@ -0,0 +1,10 @@
+package org.frekele.fiscal.focus.nfe.client.repository.nfe;
+
+import java.io.Serializable;
+
+/**
+ * @author frekele - Leandro Kersting de Freitas
+ */
+public interface FocusNFeV2Repository extends Serializable {
+
+}
---
+++ src/main/java/org/frekele/fiscal/focus/nfe/client/repository/nfe/FocusNFeV2RepositoryImpl.java
@@ -0,0 +1,44 @@
+package org.frekele.fiscal.focus.nfe.client.repository.nfe;
+
+import org.frekele.fiscal.focus.nfe.client.auth.FocusNFeAuth;
+import org.frekele.fiscal.focus.nfe.client.core.FocusNFe;
+import org.frekele.fiscal.focus.nfe.client.util.FocusNFeUtils;
+import org.jboss.resteasy.client.jaxrs.ResteasyClient;
+import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
+
+import javax.inject.Inject;
+
+/**
+ * @author frekele - Leandro Kersting de Freitas
+ */
+@FocusNFe
+public class FocusNFeV2RepositoryImpl implements FocusNFeV2Repository {
+
+    private static final long serialVersionUID = 1L;
+
+    private final ResteasyClient client;
+
+    private final FocusNFeAuth auth;
+
+    @Inject
+    public FocusNFeV2RepositoryImpl(@FocusNFe ResteasyClient client, @FocusNFe FocusNFeAuth auth) {
+        FocusNFeUtils.throwInjection(client, auth);
+        FocusNFeUtils.throwAuth(auth);
+        this.client = client;
+        this.auth = auth;
+    }
+
+    public ResteasyClient getClient() {
+        return client;
+    }
+
+    public FocusNFeAuth getAuth() {
+        return auth;
+    }
+
+    public FocusNFeV2ProxyClient getProxyClient() {
+        ResteasyClient client = this.getClient();
+        ResteasyWebTarget webTarget = client.target(this.getAuth().getEnvironment().getTargetUrl());
+        return webTarget.proxy(FocusNFeV2ProxyClient.class);
+    }
+}
