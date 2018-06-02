6d8dcfdfa54a95f51f6da697df11f058ebdf9961
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-06-02 03:23:18 -0300

    BigDecimalJsonSerialize

---
+++ src/main/java/org/frekele/fiscal/focus/nfe/client/converter/serialize/BigDecimalJsonSerialize.java
@@ -0,0 +1,28 @@
+package org.frekele.fiscal.focus.nfe.client.converter.serialize;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+/**
+ * @author frekele - Leandro Kersting de Freitas
+ */
+public class BigDecimalJsonSerialize extends StdSerializer<BigDecimal> {
+
+    protected BigDecimalJsonSerialize() {
+        super(BigDecimal.class);
+    }
+
+    @Override
+    public void serialize(BigDecimal value, JsonGenerator jsonGenerator, SerializerProvider provider) throws IOException {
+        if (value != null) {
+            //se 1.475 ou 1.471 ou 1.479 vai truncar para 1.47;
+            BigDecimal valueScaled = value.setScale(2, BigDecimal.ROUND_DOWN);
+            Long numberValue = valueScaled.unscaledValue().longValue();
+            jsonGenerator.writeNumber(numberValue);
+        }
+    }
+}
---
+++ src/test/java/org/frekele/fiscal/focus/nfe/client/converter/serialize/BigDecimalJsonSerializeTest.java
@@ -0,0 +1,37 @@
+package org.frekele.fiscal.focus.nfe.client.converter.serialize;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import org.frekele.fiscal.focus.nfe.client.testng.InvokedMethodListener;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import java.math.BigDecimal;
+
+import static org.testng.Assert.*;
+
+@Listeners(InvokedMethodListener.class)
+public class BigDecimalJsonSerializeTest {
+
+    @Mock
+    JsonGenerator jsonGenerator;
+
+    @BeforeClass
+    public void beforeClass() throws Exception {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testSerialize() throws Exception {
+        BigDecimalJsonSerialize jsonSerialize = new BigDecimalJsonSerialize();
+        assertNotNull(jsonSerialize);
+
+        jsonSerialize.serialize(null, null, null);
+        jsonSerialize.serialize(null, jsonGenerator, null);
+        jsonSerialize.serialize(BigDecimal.valueOf(4565), jsonGenerator, null);
+        jsonSerialize.serialize(BigDecimal.valueOf(345675, 2), jsonGenerator, null);
+        jsonSerialize.serialize(BigDecimal.valueOf(45353453, 3), jsonGenerator, null);
+    }
+}
