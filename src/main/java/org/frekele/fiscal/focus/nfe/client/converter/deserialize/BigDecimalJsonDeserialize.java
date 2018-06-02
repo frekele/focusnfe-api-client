09befebd3bb5277ac7f0414f773c0104fcaed77b
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-06-02 03:23:02 -0300

    BigDecimalJsonDeserialize

---
+++ src/main/java/org/frekele/fiscal/focus/nfe/client/converter/deserialize/BigDecimalJsonDeserialize.java
@@ -0,0 +1,32 @@
+package org.frekele.fiscal.focus.nfe.client.converter.deserialize;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+/**
+ * @author frekele - Leandro Kersting de Freitas
+ */
+public class BigDecimalJsonDeserialize extends StdDeserializer<BigDecimal> {
+
+    private static final int SCALE = 2;
+
+    protected BigDecimalJsonDeserialize() {
+        super(BigDecimal.class);
+    }
+
+    @Override
+    public BigDecimal deserialize(JsonParser jsonParser, DeserializationContext ctxt) throws IOException, JsonProcessingException {
+        if (jsonParser != null) {
+            Long value = jsonParser.readValueAs(Long.class);
+            if (value != null) {
+                return BigDecimal.valueOf(value, SCALE);
+            }
+        }
+        return null;
+    }
+}
---
+++ src/test/java/org/frekele/fiscal/focus/nfe/client/converter/deserialize/BigDecimalJsonDeserializeTest.java
@@ -0,0 +1,48 @@
+package org.frekele.fiscal.focus.nfe.client.converter.deserialize;
+
+import com.fasterxml.jackson.core.JsonParser;
+import org.frekele.fiscal.focus.nfe.client.testng.InvokedMethodListener;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import java.math.BigDecimal;
+
+import static org.mockito.Mockito.*;
+import static org.testng.Assert.*;
+
+/**
+ * @author frekele - Leandro Kersting de Freitas
+ */
+@Listeners(InvokedMethodListener.class)
+public class BigDecimalJsonDeserializeTest {
+
+    @Mock
+    private JsonParser jsonParser;
+
+    @BeforeClass
+    public void beforeClass() throws Exception {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testDeserialize() throws Exception {
+        BigDecimalJsonDeserialize jsonDeserialize = new BigDecimalJsonDeserialize();
+        assertNotNull(jsonDeserialize);
+
+        BigDecimal resultValue = jsonDeserialize.deserialize(null, null);
+        assertNull(resultValue);
+
+        when(jsonParser.readValueAs(Long.class)).thenReturn(null);
+        resultValue = jsonDeserialize.deserialize(jsonParser, null);
+        assertNull(resultValue);
+
+        long longValue = 5432;
+        when(jsonParser.readValueAs(Long.class)).thenReturn(longValue);
+        resultValue = jsonDeserialize.deserialize(jsonParser, null);
+        assertNotNull(resultValue);
+        assertEquals(BigDecimal.valueOf(longValue, 2), resultValue);
+    }
+}
