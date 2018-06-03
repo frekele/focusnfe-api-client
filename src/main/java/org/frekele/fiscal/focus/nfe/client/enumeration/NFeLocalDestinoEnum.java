41023cfe497be8c1d8cd773f7d927d8764aa7d30
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-06-03 06:23:04 -0300

    NFeLocalDestinoEnum

---
+++ src/main/java/org/frekele/fiscal/focus/nfe/client/enumeration/NFeLocalDestinoEnum.java
@@ -0,0 +1,54 @@
+package org.frekele.fiscal.focus.nfe.client.enumeration;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.XmlValue;
+import java.util.Arrays;
+import java.util.List;
+
+@XmlType
+@XmlEnum(String.class)
+public enum NFeLocalDestinoEnum {
+
+    OPERACAO_INTERNA("0", "Operação interna"),
+    OPERACAO_INTERESTADUAL("1", "Operação interestadual"),
+    OPERACAO_EXTERIOR("2", "Operação com exterior");
+
+    private String code;
+
+    private String description;
+
+    private NFeLocalDestinoEnum(String code, String description) {
+        this.code = code;
+        this.description = description;
+    }
+
+    @JsonValue
+    @XmlValue
+    public String getCode() {
+        return this.code;
+    }
+
+    @JsonCreator
+    public static NFeLocalDestinoEnum fromCode(String value) {
+        if (value != null && value.length() != 0) {
+            for (NFeLocalDestinoEnum obj : getAll()) {
+                if (obj.code.equals(value)) {
+                    return obj;
+                }
+            }
+        }
+        return null;
+    }
+
+    public static List<NFeLocalDestinoEnum> getAll() {
+        return Arrays.asList(NFeLocalDestinoEnum.values());
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
