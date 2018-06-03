d6c13bad017e922d811b6fa09007a7f80f51bdfd
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-06-03 01:35:42 -0300

    NFeAutorizarBodyResponse

---
+++ src/main/java/org/frekele/fiscal/focus/nfe/client/model/nfe/response/body/NFeAutorizarBodyResponse.java
@@ -0,0 +1,21 @@
+package org.frekele.fiscal.focus.nfe.client.model.nfe.response.body;
+
+import org.frekele.fiscal.focus.nfe.client.core.FocusNFeEntity;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * @author frekele - Leandro Kersting de Freitas
+ */
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class NFeAutorizarBodyResponse implements FocusNFeEntity {
+
+    private static final long serialVersionUID = 1L;
+
+    public NFeAutorizarBodyResponse() {
+        super();
+    }
+}
