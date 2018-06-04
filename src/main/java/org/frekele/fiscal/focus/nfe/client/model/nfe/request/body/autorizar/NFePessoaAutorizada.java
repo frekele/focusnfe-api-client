08e886924ee4b2564f63e332f89de2eecb9b7ccc
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-06-04 07:55:06 -0300

    NFePessoaAutorizada

---
+++ src/main/java/org/frekele/fiscal/focus/nfe/client/model/nfe/request/body/autorizar/NFePessoaAutorizada.java
@@ -0,0 +1,60 @@
+package org.frekele.fiscal.focus.nfe.client.model.nfe.request.body.autorizar;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.frekele.fiscal.focus.nfe.client.core.FocusNFeEntity;
+
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * @author frekele - Leandro Kersting de Freitas
+ */
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class NFePessoaAutorizada implements FocusNFeEntity {
+
+    private static final long serialVersionUID = 1L;
+
+    //CNPJ autorizado
+    @Size(max = 14)
+    @JsonProperty("cnpj")
+    private String cnpj;
+
+    //CPF autorizado
+    @Size(max = 11)
+    @JsonProperty("cpf")
+    private String cpf;
+
+    public NFePessoaAutorizada() {
+        super();
+    }
+
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+}
