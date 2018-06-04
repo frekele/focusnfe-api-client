1668dc0a6b14afb59171b8a895ffa1fe95c58da3
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-06-04 07:54:26 -0300

    NFeFormaPagamento

---
+++ src/main/java/org/frekele/fiscal/focus/nfe/client/model/nfe/request/body/autorizar/NFeFormaPagamento.java
@@ -0,0 +1,48 @@
+package org.frekele.fiscal.focus.nfe.client.model.nfe.request.body.autorizar;
+
+import org.frekele.fiscal.focus.nfe.client.core.FocusNFeEntity;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * @author frekele - Leandro Kersting de Freitas
+ */
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class NFeFormaPagamento implements FocusNFeEntity {
+
+    private static final long serialVersionUID = 1L;
+
+    public NFeFormaPagamento() {
+        super();
+    }
+
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+}
