b104469da368df4f5aa0ae99ea82bf1cfbe5f39c
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-06-04 07:54:51 -0300

    NFeLacreVolumeTransportado

---
+++ src/main/java/org/frekele/fiscal/focus/nfe/client/model/nfe/request/body/autorizar/NFeLacreVolumeTransportado.java
@@ -0,0 +1,55 @@
+package org.frekele.fiscal.focus.nfe.client.model.nfe.request.body.autorizar;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.frekele.fiscal.focus.nfe.client.core.FocusNFeEntity;
+
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * @author frekele - Leandro Kersting de Freitas
+ */
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class NFeLacreVolumeTransportado implements FocusNFeEntity {
+
+    private static final long serialVersionUID = 1L;
+
+    //Número do lacre.
+    @Size(min = 1, max = 60)
+    @JsonProperty("numero")
+    private String numero;
+
+    public NFeLacreVolumeTransportado() {
+        super();
+    }
+
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+}
