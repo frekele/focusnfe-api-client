d1a15abafc222559e3befe9ac803dc8736461928
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-06-05 08:48:02 -0300

    NFeRastro

--- src/main/java/org/frekele/fiscal/focus/nfe/client/model/nfe/request/body/autorizar/NFeRastro.java
+++ src/main/java/org/frekele/fiscal/focus/nfe/client/model/nfe/request/body/autorizar/NFeRastro.java
@@ -3,6 +3,8 @@ package org.frekele.fiscal.focus.nfe.client.model.nfe.request.body.autorizar;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.frekele.fiscal.focus.nfe.client.core.FocusNFeEntity;
 
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -17,10 +19,12 @@ public class NFeRastro implements FocusNFeEntity {
     private static final long serialVersionUID = 1L;
 
     //Número do lote do produto
+    @Size(min = 1, max = 20)
     @JsonProperty("numero_lote")
     private String numero_lote;
 
     //Quantidade de produtos no Lote.
+    @Digits(integer = 8, fraction = 3)
     @JsonProperty("quantidade_lote")
     private String quantidade_lote;
 
@@ -33,6 +37,7 @@ public class NFeRastro implements FocusNFeEntity {
     private String data_validade;
 
     //Código de agregação
+    @Size(min = 1, max = 20)
     @JsonProperty("codigo_agregacao")
     private String codigo_agregacao;
 
