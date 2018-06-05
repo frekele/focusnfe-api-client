a025651c41e311c7971590248cc1a1766d074f7b
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-06-05 08:47:28 -0300

    NFeDetalheExportacao

--- src/main/java/org/frekele/fiscal/focus/nfe/client/model/nfe/request/body/autorizar/NFeDetalheExportacao.java
+++ src/main/java/org/frekele/fiscal/focus/nfe/client/model/nfe/request/body/autorizar/NFeDetalheExportacao.java
@@ -3,6 +3,8 @@ package org.frekele.fiscal.focus.nfe.client.model.nfe.request.body.autorizar;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.frekele.fiscal.focus.nfe.client.core.FocusNFeEntity;
 
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -17,18 +19,22 @@ public class NFeDetalheExportacao implements FocusNFeEntity {
     private static final long serialVersionUID = 1L;
 
     //Número do ato concessório de Drawback
+    @Size(max = 11)
     @JsonProperty("numero_drawback")
     private String numero_drawback;
 
     //Número do Registro de Exportação
+    @Size(max = 12)
     @JsonProperty("numero_re")
     private String numero_re;
 
     //Chave de Acesso da NF-e recebida para exportação
+    @Size(max = 44)
     @JsonProperty("chave_nfe")
     private String chave_nfe;
 
     //Quantidade do item efetivamente exportado
+    @Digits(integer = 11, fraction = 4)
     @JsonProperty("quantidade_exportado")
     private String quantidade_exportado;
 
