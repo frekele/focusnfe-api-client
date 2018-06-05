ed82876e169d75dca371ff2be432ec1db096f07f
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-06-05 10:28:36 -0300

    NFeModeloEcfEnum

---
+++ src/main/java/org/frekele/fiscal/focus/nfe/client/enumeration/NFeModeloEcfEnum.java
@@ -0,0 +1,54 @@
+package org.frekele.fiscal.focus.nfe.client.enumeration;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.XmlValue;
+import java.util.Arrays;
+import java.util.List;
+
+@XmlType
+@XmlEnum(String.class)
+public enum NFeModeloEcfEnum {
+
+    CUPOM_FISCAL_EMITIDO_POR_MAQUINA_REGISTRADORA_NAO_ECF("2B", "Cupom Fiscal emitido por máquina registradora (não ECF)"),
+    CUPOM_FISCAL_PDV("2C", "Cupom Fiscal PDV"),
+    CUPOM_FISCAL_EMITIDO_ECF("2D", "Cupom Fiscal (emitido por ECF) (v2.0)");
+
+    private String code;
+
+    private String description;
+
+    private NFeModeloEcfEnum(String code, String description) {
+        this.code = code;
+        this.description = description;
+    }
+
+    @JsonValue
+    @XmlValue
+    public String getCode() {
+        return this.code;
+    }
+
+    @JsonCreator
+    public static NFeModeloEcfEnum fromCode(String value) {
+        if (value != null && value.length() != 0) {
+            for (NFeModeloEcfEnum obj : getAll()) {
+                if (obj.code.equals(value)) {
+                    return obj;
+                }
+            }
+        }
+        return null;
+    }
+
+    public static List<NFeModeloEcfEnum> getAll() {
+        return Arrays.asList(NFeModeloEcfEnum.values());
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
