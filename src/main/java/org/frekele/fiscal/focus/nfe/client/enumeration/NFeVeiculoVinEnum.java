4ad9833a2e741690fb3ce520894a7bbe77991535
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-06-05 09:49:46 -0300

    NFeVeiculoVinEnum

---
+++ src/main/java/org/frekele/fiscal/focus/nfe/client/enumeration/NFeVeiculoVinEnum.java
@@ -0,0 +1,53 @@
+package org.frekele.fiscal.focus.nfe.client.enumeration;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.XmlValue;
+import java.util.Arrays;
+import java.util.List;
+
+@XmlType
+@XmlEnum(String.class)
+public enum NFeVeiculoVinEnum {
+
+    REMARCADO("R", "Remarcado"),
+    NORMAL("N", "Normal");
+
+    private String code;
+
+    private String description;
+
+    private NFeVeiculoVinEnum(String code, String description) {
+        this.code = code;
+        this.description = description;
+    }
+
+    @JsonValue
+    @XmlValue
+    public String getCode() {
+        return this.code;
+    }
+
+    @JsonCreator
+    public static NFeVeiculoVinEnum fromCode(String value) {
+        if (value != null && value.length() != 0) {
+            for (NFeVeiculoVinEnum obj : getAll()) {
+                if (obj.code.equals(value)) {
+                    return obj;
+                }
+            }
+        }
+        return null;
+    }
+
+    public static List<NFeVeiculoVinEnum> getAll() {
+        return Arrays.asList(NFeVeiculoVinEnum.values());
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
