6c5f30d673284917688fea5597761d4607d2e2be
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-06-05 09:47:05 -0300

    NFeIcmsModalidadeBaseCalculoStEnum

---
+++ src/main/java/org/frekele/fiscal/focus/nfe/client/enumeration/NFeIcmsModalidadeBaseCalculoStEnum.java
@@ -0,0 +1,57 @@
+package org.frekele.fiscal.focus.nfe.client.enumeration;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.XmlValue;
+import java.util.Arrays;
+import java.util.List;
+
+@XmlType
+@XmlEnum(String.class)
+public enum NFeIcmsModalidadeBaseCalculoStEnum {
+
+    PRECO_TABELADO_OU_MAXIMO_SUGERIDO("0", "Preço tabelado ou máximo sugerido"),
+    LISTA_NEGATIVA("1", "Lista negativa (valor)"),
+    LISTA_POSITIVA("2", "Lista positiva (valor)"),
+    LISTA_NEGRA("3", "Lista neutra (valor)"),
+    MARGEM_DE_VALOR_AGREGADO("4", "Margem de valor agregado (%)"),
+    PAUTA("5", "Pauta (valor)");
+
+    private String code;
+
+    private String description;
+
+    private NFeIcmsModalidadeBaseCalculoStEnum(String code, String description) {
+        this.code = code;
+        this.description = description;
+    }
+
+    @JsonValue
+    @XmlValue
+    public String getCode() {
+        return this.code;
+    }
+
+    @JsonCreator
+    public static NFeIcmsModalidadeBaseCalculoStEnum fromCode(String value) {
+        if (value != null && value.length() != 0) {
+            for (NFeIcmsModalidadeBaseCalculoStEnum obj : getAll()) {
+                if (obj.code.equals(value)) {
+                    return obj;
+                }
+            }
+        }
+        return null;
+    }
+
+    public static List<NFeIcmsModalidadeBaseCalculoStEnum> getAll() {
+        return Arrays.asList(NFeIcmsModalidadeBaseCalculoStEnum.values());
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
