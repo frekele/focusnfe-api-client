fc08a6d54b666835101c2cd0e6d50605a937d11d
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-06-07 07:44:21 -0300

    emails

--- src/main/java/org/frekele/fiscal/focus/nfe/client/model/nfe/request/body/NFeEmailBodyRequest.java
+++ src/main/java/org/frekele/fiscal/focus/nfe/client/model/nfe/request/body/NFeEmailBodyRequest.java
@@ -1,7 +1,10 @@
 package org.frekele.fiscal.focus.nfe.client.model.nfe.request.body;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.frekele.fiscal.focus.nfe.client.core.FocusNFeEntity;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -16,6 +19,10 @@ public class NFeEmailBodyRequest implements FocusNFeEntity {
 
     private static final long serialVersionUID = 1L;
 
+    //Array com uma lista de emails que deverão receber uma cópia da nota. Limitado a 10 emails por vez.
+    @NotNull
+    @Size(min = 1, max = 10)
+    @JsonProperty("emails")
     private List<String> emails;
 
     public NFeEmailBodyRequest() {
