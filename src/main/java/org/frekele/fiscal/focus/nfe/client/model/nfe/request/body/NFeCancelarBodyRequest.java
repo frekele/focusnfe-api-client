350d93fd6bb4678fee3c4bc892b21e5b348f72a1
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-06-07 07:43:29 -0300

    justificativa

--- src/main/java/org/frekele/fiscal/focus/nfe/client/model/nfe/request/body/NFeCancelarBodyRequest.java
+++ src/main/java/org/frekele/fiscal/focus/nfe/client/model/nfe/request/body/NFeCancelarBodyRequest.java
@@ -2,6 +2,8 @@ package org.frekele.fiscal.focus.nfe.client.model.nfe.request.body;
 
 import org.frekele.fiscal.focus.nfe.client.core.FocusNFeEntity;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -15,6 +17,9 @@ public class NFeCancelarBodyRequest implements FocusNFeEntity {
 
     private static final long serialVersionUID = 1L;
 
+    //Justificativa do cancelamento. Deverá conter de 15 a 255 caracteres.
+    @NotNull
+    @Size(min = 15, max = 255)
     private String justificativa;
 
     public NFeCancelarBodyRequest() {
