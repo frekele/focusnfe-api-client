7fbfa2b6ca8aab853a570bcda02b476440a8d9c9
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-06-07 07:09:48 -0300

    LocalDateJsonConverter

---
+++ src/main/java/org/frekele/fiscal/focus/nfe/client/converter/LocalDateJsonConverter.java
@@ -0,0 +1,22 @@
+package org.frekele.fiscal.focus.nfe.client.converter;
+
+import com.fasterxml.jackson.annotation.JacksonAnnotationsInside;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import org.frekele.fiscal.focus.nfe.client.converter.deserialize.LocalDateJsonDeserialize;
+import org.frekele.fiscal.focus.nfe.client.converter.serialize.LocalDateJsonSerialize;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * @author frekele - Leandro Kersting de Freitas
+ */
+
+@Retention(RetentionPolicy.RUNTIME)
+@JacksonAnnotationsInside
+@JsonDeserialize(using = LocalDateJsonDeserialize.class)
+@JsonSerialize(using = LocalDateJsonSerialize.class)
+public @interface LocalDateJsonConverter {
+
+}
