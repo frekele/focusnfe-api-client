4a80e1818beb220216a50c2fee2210e8d3fd5d93
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-06-07 07:43:46 -0300

    correcao dataEvento

--- src/main/java/org/frekele/fiscal/focus/nfe/client/model/nfe/request/body/NFeCCeBodyRequest.java
+++ src/main/java/org/frekele/fiscal/focus/nfe/client/model/nfe/request/body/NFeCCeBodyRequest.java
@@ -1,10 +1,15 @@
 package org.frekele.fiscal.focus.nfe.client.model.nfe.request.body;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.frekele.fiscal.focus.nfe.client.converter.LocalDateJsonConverter;
 import org.frekele.fiscal.focus.nfe.client.core.FocusNFeEntity;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
+import java.time.LocalDate;
 
 /**
  * @author frekele - Leandro Kersting de Freitas
@@ -15,7 +20,16 @@ public class NFeCCeBodyRequest implements FocusNFeEntity {
 
     private static final long serialVersionUID = 1L;
 
+    //Texto da carta de correção. Deverá conter de 15 a 255 caracteres.
+    @NotNull
+    @Size(min = 15, max = 255)
+    @JsonProperty("correcao")
     private String correcao;
+
+    //Campo opcional. Data do evento da carta de correção. Se não informado será usado a data atual.
+    @LocalDateJsonConverter
+    @JsonProperty("data_evento")
+    private LocalDate dataEvento;
 
     public NFeCCeBodyRequest() {
         super();
