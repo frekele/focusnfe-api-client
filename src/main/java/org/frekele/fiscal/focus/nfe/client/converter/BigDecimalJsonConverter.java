ae608e8e6673376b0491ce1e2ea25b3ba8f84338
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-06-07 07:09:41 -0300

    BigDecimalJsonConverter

---
+++ src/main/java/org/frekele/fiscal/focus/nfe/client/converter/BigDecimalJsonConverter.java
@@ -0,0 +1,22 @@
+package org.frekele.fiscal.focus.nfe.client.converter;
+
+import com.fasterxml.jackson.annotation.JacksonAnnotationsInside;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import org.frekele.fiscal.focus.nfe.client.converter.deserialize.BigDecimalJsonDeserialize;
+import org.frekele.fiscal.focus.nfe.client.converter.serialize.BigDecimalJsonSerialize;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * @author frekele - Leandro Kersting de Freitas
+ */
+
+@Retention(RetentionPolicy.RUNTIME)
+@JacksonAnnotationsInside
+@JsonDeserialize(using = BigDecimalJsonDeserialize.class)
+@JsonSerialize(using = BigDecimalJsonSerialize.class)
+public @interface BigDecimalJsonConverter {
+
+}
