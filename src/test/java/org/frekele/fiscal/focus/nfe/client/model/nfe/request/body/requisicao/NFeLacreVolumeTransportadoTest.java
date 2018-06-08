7ce446b56aa7c9ae89e3e65c2e996bb2897d543b
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-06-08 16:09:24 -0300

    NFeLacreVolumeTransportadoTest

---
+++ src/test/java/org/frekele/fiscal/focus/nfe/client/model/nfe/request/body/requisicao/NFeLacreVolumeTransportadoTest.java
@@ -0,0 +1,47 @@
+package org.frekele.fiscal.focus.nfe.client.model.nfe.request.body.requisicao;
+
+import org.frekele.fiscal.focus.nfe.client.testng.InvokedMethodListener;
+import org.frekele.fiscal.focus.nfe.client.util.FocusNFeUtils;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import javax.validation.ConstraintViolationException;
+
+import static org.testng.Assert.*;
+
+/**
+ * @author frekele - Leandro Kersting de Freitas
+ */
+@Listeners(InvokedMethodListener.class)
+public class NFeLacreVolumeTransportadoTest {
+
+    @Test
+    public void testNewInstance() throws Exception {
+        String numero = "2798594759483";
+
+        NFeLacreVolumeTransportado entity = new NFeLacreVolumeTransportado();
+        entity.setNumero(numero);
+
+        assertNotNull(entity);
+        assertEquals(numero, entity.getNumero());
+
+        entity = NFeLacreVolumeTransportado.newBuilder()
+            .withNumero(numero)
+            .build();
+
+        assertNotNull(entity);
+        assertEquals(numero, entity.getNumero());
+    }
+
+    @Test
+    public void testBeanValidation() throws Exception {
+        NFeLacreVolumeTransportado entity = new NFeLacreVolumeTransportado();
+        FocusNFeUtils.throwBeanValidation(entity);
+    }
+
+    @Test(expectedExceptions = {ConstraintViolationException.class})
+    public void testBeanValidationWithError() throws Exception {
+        NFeLacreVolumeTransportado entity = new NFeLacreVolumeTransportado("");
+        FocusNFeUtils.throwBeanValidation(entity);
+    }
+}
