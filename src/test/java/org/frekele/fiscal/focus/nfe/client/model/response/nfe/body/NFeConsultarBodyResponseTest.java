d786d82e89f400fe7aa08727f727b9d197fd14f1
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-06-12 21:52:45 -0300

    NFeConsultarBodyResponseTest

---
+++ src/test/java/org/frekele/fiscal/focus/nfe/client/model/response/nfe/body/NFeConsultarBodyResponseTest.java
@@ -0,0 +1,33 @@
+package org.frekele.fiscal.focus.nfe.client.model.response.nfe.body;
+
+import org.frekele.fiscal.focus.nfe.client.model.entities.requisicao.NFeRequisicao;
+import org.frekele.fiscal.focus.nfe.client.testng.InvokedMethodListener;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * @author frekele - Leandro Kersting de Freitas
+ */
+@Listeners(InvokedMethodListener.class)
+public class NFeConsultarBodyResponseTest {
+
+//    @Test
+//    public void testNewInstance() throws Exception {
+//        NFeRequisicao nFeRequisicao = new NFeRequisicao();
+//
+//        NFeConsultarBodyResponse entity = new NFeConsultarBodyResponse();
+//        //entity.setNfe(nFeRequisicao);
+//
+//        assertNotNull(entity);
+//        //assertEquals(nFeRequisicao, entity.getNfe());
+//
+//        entity = NFeConsultarBodyResponse.newBuilder()
+//            //   .withNfe(nFeRequisicao)
+//            .build();
+//
+//        assertNotNull(entity);
+//        //assertEquals(nFeRequisicao, entity.getNfe());
+//    }
+}
