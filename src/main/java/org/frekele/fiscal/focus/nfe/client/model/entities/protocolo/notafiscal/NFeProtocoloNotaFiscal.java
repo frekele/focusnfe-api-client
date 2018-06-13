02d2e837f3d115eefdcfa267fa1d43e338b91f93
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-06-13 17:29:11 -0300

    NFeProtocoloNotaFiscal

--- src/main/java/org/frekele/fiscal/focus/nfe/client/model/entities/protocolo/notafiscal/NFeProtocoloNotaFiscal.java
+++ src/main/java/org/frekele/fiscal/focus/nfe/client/model/entities/protocolo/notafiscal/NFeProtocoloNotaFiscal.java
@@ -1,11 +1,14 @@
 package org.frekele.fiscal.focus.nfe.client.model.entities.protocolo.notafiscal;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.frekele.fiscal.focus.nfe.client.converter.OffsetDateTimeJsonConverter;
 import org.frekele.fiscal.focus.nfe.client.core.FocusNFeEntity;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
+import java.time.OffsetDateTime;
 
 /**
  * @author frekele - Leandro Kersting de Freitas
@@ -16,6 +19,35 @@ import javax.xml.bind.annotation.XmlRootElement;
 public class NFeProtocoloNotaFiscal implements FocusNFeEntity {
 
     private static final long serialVersionUID = 1L;
+
+    @JsonProperty("versao")
+    private String versao;
+
+    @JsonProperty("ambiente")
+    private String ambiente;
+
+    @JsonProperty("versao_aplicativo")
+    private String versaoAplicativo;
+
+    @JsonProperty("chave_nfe")
+    private String chaveNfe;
+
+    //Formato padrão ISO, exemplo: “2016-12-25T12:00-0300”.
+    @OffsetDateTimeJsonConverter
+    @JsonProperty("data_recebimento")
+    private OffsetDateTime dataRecebimento;
+
+    @JsonProperty("numero_protocolo")
+    private String numeroProtocolo;
+
+    @JsonProperty("digest_value")
+    private String digestValue;
+
+    @JsonProperty("status")
+    private String status;
+
+    @JsonProperty("motivo")
+    private String motivo;
 
     public NFeProtocoloNotaFiscal() {
         super();
