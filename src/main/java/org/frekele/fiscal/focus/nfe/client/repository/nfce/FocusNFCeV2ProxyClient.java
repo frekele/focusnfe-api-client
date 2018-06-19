2e2816240117b7f34eb0a53635f06cdff69831e0
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-06-18 22:26:49 -0300

    FocusNFCeV2ProxyClient

--- src/main/java/org/frekele/fiscal/focus/nfe/client/repository/nfce/FocusNFCeV2ProxyClient.java
+++ src/main/java/org/frekele/fiscal/focus/nfe/client/repository/nfce/FocusNFCeV2ProxyClient.java
@@ -1,6 +1,14 @@
 package org.frekele.fiscal.focus.nfe.client.repository.nfce;
 
-import org.frekele.fiscal.focus.nfe.client.core.FocusNFeEntity;
+import org.frekele.fiscal.focus.nfe.client.model.request.nfce.body.NFCeAutorizarBodyRequest;
+import org.frekele.fiscal.focus.nfe.client.model.request.nfce.body.NFCeCancelarBodyRequest;
+import org.frekele.fiscal.focus.nfe.client.model.request.nfce.body.NFCeEmailBodyRequest;
+import org.frekele.fiscal.focus.nfe.client.model.request.nfce.body.NFCeInutilizarBodyRequest;
+import org.frekele.fiscal.focus.nfe.client.model.response.nfce.NFCeAutorizarResponse;
+import org.frekele.fiscal.focus.nfe.client.model.response.nfce.NFCeCancelarResponse;
+import org.frekele.fiscal.focus.nfe.client.model.response.nfce.NFCeConsultarResponse;
+import org.frekele.fiscal.focus.nfe.client.model.response.nfce.NFCeEmailResponse;
+import org.frekele.fiscal.focus.nfe.client.model.response.nfce.NFCeInutilizarResponse;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
@@ -24,64 +32,68 @@ interface FocusNFCeV2ProxyClient extends Serializable {
 
     /**
      * POST - Emitir uma Nota Fiscal de Consumidor Eletrônica (NFCe) para qualquer Estado que aceita o uso deste documento.
-     * Todos os processos envolvendo NFCe são síncronos.
+     * Todos os processos envolvendo NFCe são Síncronos.
      * Cria uma nota fiscal e a envia para processamento.
-     * Exemplo de requisição: POST http://homologacao.acrasnfe.acras.com.br/v2/nfce
+     * Exemplo de requisição: POST https://api.focusnfe.com.br/v2/nfce?ref=REFERENCIA
      */
     @POST
     @Path("nfce")
-    FocusNFeEntity send(@HeaderParam("Authorization") String authorization,
-                        @QueryParam("ref") String referencia);
+    NFCeAutorizarResponse autorizar(@HeaderParam("Authorization") String authorization,
+                                    @QueryParam("ref") String referencia,
+                                    NFCeAutorizarBodyRequest bodyRequest);
 
     /**
-     * GET - Consultar NFCe emitidas.
+     * GET - Consultar o status de NFCe emitidas.
      * Este processo é assíncrono. Ou seja, após a emissão a nota será enfileirada para processamento.
      * Consulta a nota fiscal com a referência informada.
-     * Exemplo de requisição: GET http://homologacao.acrasnfe.acras.com.br/v2/nfce/12345
+     * Exemplo de requisição: GET https://api.focusnfe.com.br/v2/nfce/REFERENCIA
      */
     @GET
     @Path("nfce/{referencia}")
-    FocusNFeEntity find(@HeaderParam("Authorization") String authorization,
-                        @PathParam("referencia") String referencia);
+    NFCeConsultarResponse consultar(@HeaderParam("Authorization") String authorization,
+                                    @PathParam("referencia") String referencia);
 
     /**
-     * GET - Consultar NFCe emitidas.
+     * GET - Consultar o status de NFCe emitidas.
      * Este processo é assíncrono. Ou seja, após a emissão a nota será enfileirada para processamento.
      * Consulta a nota fiscal com a referência informada e o seu status de processamento.
-     * Exemplo de requisição: GET http://homologacao.acrasnfe.acras.com.br/v2/nfce/12345?completa=1
+     * Exemplo de requisição: GET https://api.focusnfe.com.br/v2/nfce/REFERENCIA?completa=(0|1)
      */
     @GET
     @Path("nfce/{referencia}")
-    FocusNFeEntity find(@HeaderParam("Authorization") String authorization,
-                        @PathParam("referencia") String referencia,
-                        @QueryParam("completa") String completa);
+    NFCeConsultarResponse consultar(@HeaderParam("Authorization") String authorization,
+                                    @PathParam("referencia") String referencia,
+                                    @QueryParam("completa") Integer completa);
 
     /**
      * DELETE - Cancelar NFCe.
      * Cancela uma nota fiscal com a referência informada.
-     * Exemplo de requisição: DELETE http://homologacao.acrasnfe.acras.com.br/v2/nfce/12345
+     * Exemplo de requisição: DELETE https://api.focusnfe.com.br/v2/nfce/REFERENCIA
      */
     @DELETE
     @Path("nfce/{referencia}")
-    FocusNFeEntity cancel(@HeaderParam("Authorization") String authorization,
-                          @PathParam("referencia") String referencia);
+    NFCeCancelarResponse cancelar(@HeaderParam("Authorization") String authorization,
+                                  @PathParam("referencia") String referencia,
+                                  NFCeCancelarBodyRequest bodyRequest);
 
     /**
      * POST - Encaminhar uma NFCe por email.
      * Envia um email com uma cópia da nota fiscal com a referência informada.
-     * Exemplo de requisição: POST http://homologacao.acrasnfe.acras.com.br/v2/nfce/12345/email
+     * Exemplo de requisição: POST https://api.focusnfe.com.br/v2/nfce/REFERENCIA/email
      */
     @POST
     @Path("nfce/{referencia}/email")
-    FocusNFeEntity sendEmail(@HeaderParam("Authorization") String authorization,
-                             @PathParam("referencia") String referencia);
+    NFCeEmailResponse enviarEmail(@HeaderParam("Authorization") String authorization,
+                                  @PathParam("referencia") String referencia,
+                                  NFCeEmailBodyRequest bodyRequest);
 
     /**
-     * POST - Inutilizar o número de alguma série de NFCe.
+     * POST - Inutilizar uma faixa de numeração de NFCe.
      * Inutiliza uma numeração da nota fiscal.
-     * Exemplo de requisição: POST http://homologacao.acrasnfe.acras.com.br/v2/nfce/inutilizacao
+     * Exemplo de requisição: POST https://api.focusnfe.com.br/v2/nfce/inutilizacao
      */
     @POST
     @Path("nfce/inutilizacao")
-    FocusNFeEntity disablement(@HeaderParam("Authorization") String authorization);
+    NFCeInutilizarResponse inutilizar(@HeaderParam("Authorization") String authorization,
+                                      NFCeInutilizarBodyRequest bodyRequest);
 }
