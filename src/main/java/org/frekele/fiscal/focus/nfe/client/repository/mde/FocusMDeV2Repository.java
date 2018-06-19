13f6ee0da111d0cc67d638d6803ed153ce1dd542
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-06-19 00:33:41 -0300

    FocusMDeV2Repository

---
+++ src/main/java/org/frekele/fiscal/focus/nfe/client/repository/mde/FocusMDeV2Repository.java
@@ -0,0 +1,10 @@
+package org.frekele.fiscal.focus.nfe.client.repository.mde;
+
+import java.io.Serializable;
+
+/**
+ * @author frekele - Leandro Kersting de Freitas
+ */
+public interface FocusMDeV2Repository extends Serializable {
+
+}
