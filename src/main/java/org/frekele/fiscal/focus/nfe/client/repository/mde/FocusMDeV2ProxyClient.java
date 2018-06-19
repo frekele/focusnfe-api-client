b81eca7e944c90798cfbc0de88715ce76bfef952
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-06-19 16:37:47 -0300

    manifestar

--- src/main/java/org/frekele/fiscal/focus/nfe/client/repository/mde/FocusMDeV2ProxyClient.java
+++ src/main/java/org/frekele/fiscal/focus/nfe/client/repository/mde/FocusMDeV2ProxyClient.java
@@ -1,13 +1,14 @@
 package org.frekele.fiscal.focus.nfe.client.repository.mde;
 
-import org.frekele.fiscal.focus.nfe.client.core.FocusNFeEntity;
+import org.frekele.fiscal.focus.nfe.client.model.request.mde.MDeManifestarBodyRequest;
+import org.frekele.fiscal.focus.nfe.client.model.response.nfce.NFCeAutorizarResponse;
 
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import java.io.Serializable;
 
@@ -20,13 +21,12 @@ import java.io.Serializable;
 interface FocusMDeV2ProxyClient extends Serializable {
 
     /**
-     * GET - Consultar o status de NFCe emitidas.
-     * Este processo é assíncrono. Ou seja, após a emissão a nota será enfileirada para processamento.
-     * Consulta a nota fiscal com a referência informada.
-     * Exemplo de requisição: GET https://api.focusnfe.com.br/v2/nfce/REFERENCIA
+     * POST - Realiza um manifesto na nota informada.
+     * Exemplo de requisição: POST https://api.focusnfe.com.br/v2/nfes_recebidas/CHAVE_NFE/manifesto
      */
-    @GET
-    @Path("xxxx/{referencia}")
-    FocusNFeEntity consultar(@HeaderParam("Authorization") String authorization,
-                             @PathParam("referencia") String referencia);
+    @POST
+    @Path("nfes_recebidas/{chaveNFe}/manifesto")
+    NFCeAutorizarResponse manifestar(@HeaderParam("Authorization") String authorization,
+                                      @QueryParam("chaveNFe") String chaveNFe,
+                                      MDeManifestarBodyRequest bodyRequest);
 }
