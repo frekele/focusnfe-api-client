32b1d883a7a4609c29a74d1999396a7a10697209
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-06-20 19:09:31 -0300

    consultarTudo

--- src/main/java/org/frekele/fiscal/focus/nfe/client/repository/mde/FocusMDeV2ProxyClient.java
+++ src/main/java/org/frekele/fiscal/focus/nfe/client/repository/mde/FocusMDeV2ProxyClient.java
@@ -4,9 +4,11 @@ import org.frekele.fiscal.focus.nfe.client.model.request.mde.MDeManifestarBodyRe
 import org.frekele.fiscal.focus.nfe.client.model.response.mde.MDeManifestarResponse;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
@@ -27,6 +29,36 @@ interface FocusMDeV2ProxyClient extends Serializable {
     @POST
     @Path("nfes_recebidas/{chaveNFe}/manifesto")
     MDeManifestarResponse manifestar(@HeaderParam("Authorization") String authorization,
-                                     @QueryParam("chaveNFe") String chaveNFe,
+                                     @PathParam("chaveNFe") String chaveNFe,
                                      MDeManifestarBodyRequest bodyRequest);
+
+    /**
+     * GET - Busca informações resumidas de todas as NFe’s recebidas.
+     * Exemplo de requisição: POST https://api.focusnfe.com.br/v2/v2/nfes_recebidas?cnpj=CNPJ
+     */
+    @GET
+    @Path("nfes_recebidas")
+    MDeManifestarResponse consultarTudo(@HeaderParam("Authorization") String authorization,
+                                        @QueryParam("cnpj") String cnpj);
+
+    /**
+     * GET - Busca informações resumidas de todas as NFe’s recebidas.
+     * Exemplo de requisição: POST https://api.focusnfe.com.br/v2/v2/nfes_recebidas?cnpj=CNPJ&versao=VERSAO
+     */
+    @GET
+    @Path("nfes_recebidas")
+    MDeManifestarResponse consultarTudo(@HeaderParam("Authorization") String authorization,
+                                        @QueryParam("cnpj") String cnpj,
+                                        @QueryParam("versao") String versao);
+
+    /**
+     * GET - Busca informações resumidas de todas as NFe’s recebidas.
+     * Exemplo de requisição: POST https://api.focusnfe.com.br/v2/v2/nfes_recebidas?cnpj=CNPJ&versao=VERSAO&pendente=PENDENTE
+     */
+    @GET
+    @Path("nfes_recebidas")
+    MDeManifestarResponse consultarTudo(@HeaderParam("Authorization") String authorization,
+                                        @QueryParam("cnpj") String cnpj,
+                                        @QueryParam("versao") String versao,
+                                        @QueryParam("pendente") String pendente);
 }
