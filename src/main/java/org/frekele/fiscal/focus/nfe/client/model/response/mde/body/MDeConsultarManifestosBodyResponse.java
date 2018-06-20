9a41a2645d09ad686db9eb5ddcabfde6d3250fcd
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-06-20 19:54:25 -0300

    MDeConsultarManifestosBodyResponse

---
+++ src/main/java/org/frekele/fiscal/focus/nfe/client/model/response/mde/body/MDeConsultarManifestosBodyResponse.java
@@ -0,0 +1,49 @@
+package org.frekele.fiscal.focus.nfe.client.model.response.mde.body;
+
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import org.frekele.fiscal.focus.nfe.client.core.FocusNFeEntity;
+import org.frekele.fiscal.focus.nfe.client.model.entities.erro.NFeErro;
+import org.frekele.fiscal.focus.nfe.client.model.entities.manifesto.MDeManifesto;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
+
+/**
+ * @author frekele - Leandro Kersting de Freitas
+ */
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class MDeConsultarManifestosBodyResponse implements FocusNFeEntity {
+
+    private static final long serialVersionUID = 1L;
+
+    //As mensagens de erro serão apresentadas em qualquer operação sempre que for devolvido um código HTTP que começa com 4.
+    @JsonUnwrapped
+    private NFeErro erro;
+
+    ////Lista de Manifestações emitidas para o CNPJ;
+    @JsonUnwrapped
+    private List<MDeManifesto> manifestos;
+
+    public MDeConsultarManifestosBodyResponse() {
+        super();
+    }
+
+    public NFeErro getErro() {
+        return erro;
+    }
+
+    public void setErro(NFeErro erro) {
+        this.erro = erro;
+    }
+
+    public List<MDeManifesto> getManifestos() {
+        return manifestos;
+    }
+
+    public void setManifestos(List<MDeManifesto> manifestos) {
+        this.manifestos = manifestos;
+    }
+}
