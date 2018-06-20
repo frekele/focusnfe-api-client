8937257e4ff93163c5c64f7cd691a9d8e09062f4
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-06-20 20:17:03 -0300

    FocusMDeV2RepositoryIT

---
+++ src/test/java/org/frekele/fiscal/focus/nfe/client/repository/mde/FocusMDeV2RepositoryIT.java
@@ -0,0 +1,12 @@
+package org.frekele.fiscal.focus.nfe.client.repository.mde;
+
+import org.frekele.fiscal.focus.nfe.client.testng.InvokedMethodListener;
+import org.testng.annotations.Listeners;
+
+/**
+ * @author frekele - Leandro Kersting de Freitas
+ */
+@Listeners(InvokedMethodListener.class)
+public class FocusMDeV2RepositoryIT {
+
+}
