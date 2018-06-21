e58d4b2bc1029df81f38865d38ef2fa170d448f6
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-06-20 21:20:16 -0300

    MDeConsultarNFeResponse

---
+++ src/main/java/org/frekele/fiscal/focus/nfe/client/model/response/mde/MDeConsultarNFeResponse.java
@@ -0,0 +1,34 @@
+package org.frekele.fiscal.focus.nfe.client.model.response.mde;
+
+import org.frekele.fiscal.focus.nfe.client.core.FocusNFeEntity;
+import org.frekele.fiscal.focus.nfe.client.model.response.mde.body.MDeConsultarNFeBodyResponse;
+import org.jboss.resteasy.annotations.Body;
+import org.jboss.resteasy.annotations.ResponseObject;
+import org.jboss.resteasy.annotations.Status;
+
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.core.Response;
+
+/**
+ * @author frekele - Leandro Kersting de Freitas
+ */
+@ResponseObject
+public interface MDeConsultarNFeResponse extends FocusNFeEntity {
+
+    @HeaderParam("X-Rate-Limit-Limit")
+    String getRateLimitLimit();
+
+    @HeaderParam("X-Rate-Limit-Remaining")
+    String getRateLimitRemaining();
+
+    @HeaderParam("X-Rate-Limit-Reset")
+    String getRateLimitReset();
+
+    @Status
+    Integer getStatus();
+
+    @Body
+    MDeConsultarNFeBodyResponse getBody();
+
+    Response getResponse();
+}
