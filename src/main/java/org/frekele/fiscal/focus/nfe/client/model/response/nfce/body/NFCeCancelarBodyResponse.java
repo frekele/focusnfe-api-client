9e9148ace583e99c293358b12cb4bd6fb8baddf1
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-06-24 07:51:56 -0300

    javadoc.

--- src/main/java/org/frekele/fiscal/focus/nfe/client/model/response/nfce/body/NFCeCancelarBodyResponse.java
+++ src/main/java/org/frekele/fiscal/focus/nfe/client/model/response/nfce/body/NFCeCancelarBodyResponse.java
@@ -18,23 +18,33 @@ public class NFCeCancelarBodyResponse implements FocusNFeEntity {
 
     private static final long serialVersionUID = 1L;
 
-    //As mensagens de erro serão apresentadas em qualquer operação sempre que for devolvido um código HTTP que começa com 4.
+    /**
+     * As mensagens de erro serão apresentadas em qualquer operação sempre que for devolvido um código HTTP que começa com 4.
+     */
     @JsonUnwrapped
     private NFeErro erro;
 
-    //Cancelado, se a nota pode ser cancelada, ou erro_cancelamento, se houve algum erro ao cancelar a nota.
+    /**
+     * Cancelado, se a nota pode ser cancelada, ou erro_cancelamento, se houve algum erro ao cancelar a nota.
+     */
     @JsonProperty("status")
     private String status;
 
-    //O status do cancelamento na SEFAZ.
+    /**
+     * O status do cancelamento na SEFAZ.
+     */
     @JsonProperty("status_sefaz")
     private String statusSefaz;
 
-    //Mensagem descritiva da SEFAZ detalhando o status.
+    /**
+     * Mensagem descritiva da SEFAZ detalhando o status.
+     */
     @JsonProperty("mensagem_sefaz")
     private String mensagemSefaz;
 
-    //Caso a nota tenha sido cancelada, será informado aqui o caminho para download do XML de cancelamento.
+    /**
+     * Caso a nota tenha sido cancelada, será informado aqui o caminho para download do XML de cancelamento.
+     */
     @JsonProperty("caminho_xml_cancelamento")
     private String caminhoXmlCancelamento;
 
@@ -42,42 +52,72 @@ public class NFCeCancelarBodyResponse implements FocusNFeEntity {
         super();
     }
 
+    /**
+     * As mensagens de erro serão apresentadas em qualquer operação sempre que for devolvido um código HTTP que começa com 4.
+     */
     public NFeErro getErro() {
         return erro;
     }
 
+    /**
+     * As mensagens de erro serão apresentadas em qualquer operação sempre que for devolvido um código HTTP que começa com 4.
+     */
     public void setErro(NFeErro erro) {
         this.erro = erro;
     }
 
+    /**
+     * Cancelado, se a nota pode ser cancelada, ou erro_cancelamento, se houve algum erro ao cancelar a nota.
+     */
     public String getStatus() {
         return status;
     }
 
+    /**
+     * Cancelado, se a nota pode ser cancelada, ou erro_cancelamento, se houve algum erro ao cancelar a nota.
+     */
     public void setStatus(String status) {
         this.status = status;
     }
 
+    /**
+     * O status do cancelamento na SEFAZ.
+     */
     public String getStatusSefaz() {
         return statusSefaz;
     }
 
+    /**
+     * O status do cancelamento na SEFAZ.
+     */
     public void setStatusSefaz(String statusSefaz) {
         this.statusSefaz = statusSefaz;
     }
 
+    /**
+     * Mensagem descritiva da SEFAZ detalhando o status.
+     */
     public String getMensagemSefaz() {
         return mensagemSefaz;
     }
 
+    /**
+     * Mensagem descritiva da SEFAZ detalhando o status.
+     */
     public void setMensagemSefaz(String mensagemSefaz) {
         this.mensagemSefaz = mensagemSefaz;
     }
 
+    /**
+     * Caso a nota tenha sido cancelada, será informado aqui o caminho para download do XML de cancelamento.
+     */
     public String getCaminhoXmlCancelamento() {
         return caminhoXmlCancelamento;
     }
 
+    /**
+     * Caso a nota tenha sido cancelada, será informado aqui o caminho para download do XML de cancelamento.
+     */
     public void setCaminhoXmlCancelamento(String caminhoXmlCancelamento) {
         this.caminhoXmlCancelamento = caminhoXmlCancelamento;
     }
--- src/main/java/org/frekele/fiscal/focus/nfe/client/model/response/nfce/body/NFCeConsultarBodyResponse.java
+++ src/main/java/org/frekele/fiscal/focus/nfe/client/model/response/nfce/body/NFCeConsultarBodyResponse.java
@@ -22,68 +22,103 @@ public class NFCeConsultarBodyResponse implements FocusNFeEntity {
 
     private static final long serialVersionUID = 1L;
 
-    //As mensagens de erro serão apresentadas em qualquer operação sempre que for devolvido um código HTTP que começa com 4.
+    /**
+     * As mensagens de erro serão apresentadas em qualquer operação sempre que for devolvido um código HTTP que começa com 4.
+     */
     @JsonUnwrapped
     private NFeErro erro;
 
-    // A situação da NFe, podendo ser: autorizado, cancelado, erro_autorizacao ou denegado.
+    /**
+     * A situação da NFe, podendo ser: autorizado, cancelado, erro_autorizacao ou denegado.
+     */
     @JsonProperty("status")
     private String status;
 
-    //O status da nota na SEFAZ.
+    /**
+     * O status da nota na SEFAZ.
+     */
     @JsonProperty("status_sefaz")
     private String statusSefaz;
 
-    //Mensagem descritiva da SEFAZ detalhando o status.
+    /**
+     * Mensagem descritiva da SEFAZ detalhando o status.
+     */
     @JsonProperty("mensagem_sefaz")
     private String mensagemSefaz;
 
-    //A série da nota fiscal, caso ela tenha sido autorizada.
+    /**
+     * A série da nota fiscal, caso ela tenha sido autorizada.
+     */
     @JsonProperty("serie")
     private String serie;
 
-    //O número da nota fiscal, caso ela tenha sido autorizada.
+    /**
+     * O número da nota fiscal, caso ela tenha sido autorizada.
+     */
     @JsonProperty("numero")
     private String numero;
 
-    //O CNPJ emitente da nota fiscal (o CNPJ de sua empresa).
+    /**
+     * O CNPJ emitente da nota fiscal (o CNPJ de sua empresa).
+     */
     @JsonProperty("cnpj_emitente")
     private String cnpjEmitente;
 
-    //A referência da emissão.
+    /**
+     * A referência da emissão.
+     */
     @JsonProperty("ref")
     private String referencia;
 
-    //A chave da NFe, caso ela tenha sido autorizada.
+    /**
+     * A chave da NFe, caso ela tenha sido autorizada.
+     */
     @JsonProperty("chave_nfe")
     private String chaveNfe;
 
-    //Caso a nota tenha sido autorizada, retorna o caminho para download do XML.
+    /**
+     * Caso a nota tenha sido autorizada, retorna o caminho para download do XML.
+     */
     @JsonProperty("caminho_xml_nota_fiscal")
     private String caminhoXmlNotaFiscal;
 
-    //Caso a nota tenha sido autorizada retorna o caminho para download do DANFe.
+    /**
+     * Caso a nota tenha sido autorizada retorna o caminho para download do DANFe.
+     */
     @JsonProperty("caminho_danfe")
     private String caminhoDanfe;
 
-    //Caso a nota esteja cancelada, é fornecido o caminho para fazer o download do XML de cancelamento.
+    /**
+     * Caso a nota esteja cancelada, é fornecido o caminho para fazer o download do XML de cancelamento.
+     */
     @JsonProperty("caminho_xml_cancelamento")
     private String caminhoXmlCancelamento;
 
-    //Caso na requisição seja passado o parâmetro completa=1;
-    //Inclui os dados completos da requisição da nota fiscal, da mesma forma que constam no XML da nota.
+    /**
+     * Inclui os dados completos da requisição da nota fiscal, da mesma forma que constam no XML da nota.
+     * Caso na requisição seja passado o parâmetro completa=1.
+     */
     @JsonProperty("requisicao_nota_fiscal")
     private NFeRetornoRequisicaoNotaFiscal requisicaoNotaFiscal;
 
-    //Inclui os dados completos do protocolo devolvido pela SEFAZ.
+    /**
+     * Inclui os dados completos do protocolo devolvido pela SEFAZ.
+     * Caso na requisição seja passado o parâmetro completa=1.
+     */
     @JsonProperty("protocolo_nota_fiscal")
     private NFeProtocoloNotaFiscal protocoloNotaFiscal;
 
-    //Inclui os dados completos da requisição de cancelamento da nota fiscal.
+    /**
+     * Inclui os dados completos da requisição de cancelamento da nota fiscal.
+     * Caso na requisição seja passado o parâmetro completa=1.
+     */
     @JsonProperty("requisicao_cancelamento")
     private NFeRequisicaoCancelamento requisicaoCancelamento;
 
-    //Inclui os dados completos do protocolo devolvido pela SEFAZ.
+    /**
+     * Inclui os dados completos do protocolo devolvido pela SEFAZ.
+     * Caso na requisição seja passado o parâmetro completa=1.
+     */
     @JsonProperty("protocolo_cancelamento")
     private NFeProtocoloCancelamento protocoloCancelamento;
 
@@ -91,130 +126,234 @@ public class NFCeConsultarBodyResponse implements FocusNFeEntity {
         super();
     }
 
+    /**
+     * As mensagens de erro serão apresentadas em qualquer operação sempre que for devolvido um código HTTP que começa com 4.
+     */
     public NFeErro getErro() {
         return erro;
     }
 
+    /**
+     * As mensagens de erro serão apresentadas em qualquer operação sempre que for devolvido um código HTTP que começa com 4.
+     */
     public void setErro(NFeErro erro) {
         this.erro = erro;
     }
 
+    /**
+     * A situação da NFe, podendo ser: autorizado, cancelado, erro_autorizacao ou denegado.
+     */
     public String getStatus() {
         return status;
     }
 
+    /**
+     * A situação da NFe, podendo ser: autorizado, cancelado, erro_autorizacao ou denegado.
+     */
     public void setStatus(String status) {
         this.status = status;
     }
 
+    /**
+     * O status da nota na SEFAZ.
+     */
     public String getStatusSefaz() {
         return statusSefaz;
     }
 
+    /**
+     * O status da nota na SEFAZ.
+     */
     public void setStatusSefaz(String statusSefaz) {
         this.statusSefaz = statusSefaz;
     }
 
+    /**
+     * Mensagem descritiva da SEFAZ detalhando o status.
+     */
     public String getMensagemSefaz() {
         return mensagemSefaz;
     }
 
+    /**
+     * Mensagem descritiva da SEFAZ detalhando o status.
+     */
     public void setMensagemSefaz(String mensagemSefaz) {
         this.mensagemSefaz = mensagemSefaz;
     }
 
+    /**
+     * A série da nota fiscal, caso ela tenha sido autorizada.
+     */
     public String getSerie() {
         return serie;
     }
 
+    /**
+     * A série da nota fiscal, caso ela tenha sido autorizada.
+     */
     public void setSerie(String serie) {
         this.serie = serie;
     }
 
+    /**
+     * O número da nota fiscal, caso ela tenha sido autorizada.
+     */
     public String getNumero() {
         return numero;
     }
 
+    /**
+     * O número da nota fiscal, caso ela tenha sido autorizada.
+     */
     public void setNumero(String numero) {
         this.numero = numero;
     }
 
+    /**
+     * O CNPJ emitente da nota fiscal (o CNPJ de sua empresa).
+     */
     public String getCnpjEmitente() {
         return cnpjEmitente;
     }
 
+    /**
+     * O CNPJ emitente da nota fiscal (o CNPJ de sua empresa).
+     */
     public void setCnpjEmitente(String cnpjEmitente) {
         this.cnpjEmitente = cnpjEmitente;
     }
 
+    /**
+     * A referência da emissão.
+     */
     public String getReferencia() {
         return referencia;
     }
 
+    /**
+     * A referência da emissão.
+     */
     public void setReferencia(String referencia) {
         this.referencia = referencia;
     }
 
+    /**
+     * A chave da NFe, caso ela tenha sido autorizada.
+     */
     public String getChaveNfe() {
         return chaveNfe;
     }
 
+    /**
+     * A chave da NFe, caso ela tenha sido autorizada.
+     */
     public void setChaveNfe(String chaveNfe) {
         this.chaveNfe = chaveNfe;
     }
 
+    /**
+     * Caso a nota tenha sido autorizada, retorna o caminho para download do XML.
+     */
     public String getCaminhoXmlNotaFiscal() {
         return caminhoXmlNotaFiscal;
     }
 
+    /**
+     * Caso a nota tenha sido autorizada, retorna o caminho para download do XML.
+     */
     public void setCaminhoXmlNotaFiscal(String caminhoXmlNotaFiscal) {
         this.caminhoXmlNotaFiscal = caminhoXmlNotaFiscal;
     }
 
+    /**
+     * Caso a nota tenha sido autorizada retorna o caminho para download do DANFe.
+     */
     public String getCaminhoDanfe() {
         return caminhoDanfe;
     }
 
+    /**
+     * Caso a nota tenha sido autorizada retorna o caminho para download do DANFe.
+     */
     public void setCaminhoDanfe(String caminhoDanfe) {
         this.caminhoDanfe = caminhoDanfe;
     }
 
+    /**
+     * Caso a nota esteja cancelada, é fornecido o caminho para fazer o download do XML de cancelamento.
+     */
     public String getCaminhoXmlCancelamento() {
         return caminhoXmlCancelamento;
     }
 
+    /**
+     * Caso a nota esteja cancelada, é fornecido o caminho para fazer o download do XML de cancelamento.
+     */
     public void setCaminhoXmlCancelamento(String caminhoXmlCancelamento) {
         this.caminhoXmlCancelamento = caminhoXmlCancelamento;
     }
 
+    /**
+     * Inclui os dados completos da requisição da nota fiscal, da mesma forma que constam no XML da nota.
+     * Caso na requisição seja passado o parâmetro completa=1.
+     */
     public NFeRetornoRequisicaoNotaFiscal getRequisicaoNotaFiscal() {
         return requisicaoNotaFiscal;
     }
 
+    /**
+     * Inclui os dados completos da requisição da nota fiscal, da mesma forma que constam no XML da nota.
+     * Caso na requisição seja passado o parâmetro completa=1.
+     */
     public void setRequisicaoNotaFiscal(NFeRetornoRequisicaoNotaFiscal requisicaoNotaFiscal) {
         this.requisicaoNotaFiscal = requisicaoNotaFiscal;
     }
 
+    /**
+     * Inclui os dados completos do protocolo devolvido pela SEFAZ.
+     * Caso na requisição seja passado o parâmetro completa=1.
+     */
     public NFeProtocoloNotaFiscal getProtocoloNotaFiscal() {
         return protocoloNotaFiscal;
     }
 
+    /**
+     * Inclui os dados completos do protocolo devolvido pela SEFAZ.
+     * Caso na requisição seja passado o parâmetro completa=1.
+     */
     public void setProtocoloNotaFiscal(NFeProtocoloNotaFiscal protocoloNotaFiscal) {
         this.protocoloNotaFiscal = protocoloNotaFiscal;
     }
 
+    /**
+     * Inclui os dados completos da requisição de cancelamento da nota fiscal.
+     * Caso na requisição seja passado o parâmetro completa=1.
+     */
     public NFeRequisicaoCancelamento getRequisicaoCancelamento() {
         return requisicaoCancelamento;
     }
 
+    /**
+     * Inclui os dados completos da requisição de cancelamento da nota fiscal.
+     * Caso na requisição seja passado o parâmetro completa=1.
+     */
     public void setRequisicaoCancelamento(NFeRequisicaoCancelamento requisicaoCancelamento) {
         this.requisicaoCancelamento = requisicaoCancelamento;
     }
 
+    /**
+     * Inclui os dados completos do protocolo devolvido pela SEFAZ.
+     * Caso na requisição seja passado o parâmetro completa=1.
+     */
     public NFeProtocoloCancelamento getProtocoloCancelamento() {
         return protocoloCancelamento;
     }
 
+    /**
+     * Inclui os dados completos do protocolo devolvido pela SEFAZ.
+     * Caso na requisição seja passado o parâmetro completa=1.
+     */
     public void setProtocoloCancelamento(NFeProtocoloCancelamento protocoloCancelamento) {
         this.protocoloCancelamento = protocoloCancelamento;
     }
