b5e2477205833ef2c05014e9f2f459cabe79526a
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-06-25 08:08:36 -0300

    ResponseArrayJsonReplaceFilterTest

--- src/test/java/org/frekele/fiscal/focus/nfe/client/filter/ResponseArrayJsonReplaceFilterTest.java
+++ src/test/java/org/frekele/fiscal/focus/nfe/client/filter/ResponseArrayJsonReplaceFilterTest.java
@@ -1,7 +1,34 @@
 package org.frekele.fiscal.focus.nfe.client.filter;
 
-import static org.testng.Assert.*;
+import org.frekele.fiscal.focus.nfe.client.testng.InvokedMethodListener;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
 
+import javax.ws.rs.client.ClientRequestContext;
+import javax.ws.rs.client.ClientResponseContext;
+import java.net.URI;
+
+import static org.mockito.Mockito.*;
+
+/**
+ * @author frekele - Leandro Kersting de Freitas
+ */
+@Listeners(InvokedMethodListener.class)
 public class ResponseArrayJsonReplaceFilterTest {
 
+    @Test
+    public void testNewInstance() throws Exception {
+        ClientRequestContext requestContext = mock(ClientRequestContext.class);
+        ClientResponseContext responseContext = mock(ClientResponseContext.class);
+
+        new ResponseArrayJsonReplaceFilter().filter(requestContext, responseContext);
+
+        when(requestContext.getMethod()).thenReturn("GET");
+        when(requestContext.getUri()).thenReturn(new URI("/v2/nfes_recebidas?cnpj=34598375834"));
+        new ResponseArrayJsonReplaceFilter().filter(requestContext, responseContext);
+
+        when(requestContext.getMethod()).thenReturn("GET");
+        when(requestContext.getUri()).thenReturn(new URI("/v2/hooks?hooks=hooks"));
+        new ResponseArrayJsonReplaceFilter().filter(requestContext, responseContext);
+    }
 }
