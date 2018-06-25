a0500001608af12f35989903564823fb976a1f2c
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-06-25 10:18:11 -0300

    NcmsBodyResponseTest

--- src/test/java/org/frekele/fiscal/focus/nfe/client/model/response/ncm/body/NcmsBodyResponseTest.java
+++ src/test/java/org/frekele/fiscal/focus/nfe/client/model/response/ncm/body/NcmsBodyResponseTest.java
@@ -1,7 +1,33 @@
 package org.frekele.fiscal.focus.nfe.client.model.response.ncm.body;
 
+import org.frekele.fiscal.focus.nfe.client.model.entities.erro.NFeErro;
+import org.frekele.fiscal.focus.nfe.client.testng.InvokedMethodListener;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.testng.Assert.*;
 
+/**
+ * @author frekele - Leandro Kersting de Freitas
+ */
+@Listeners(InvokedMethodListener.class)
 public class NcmsBodyResponseTest {
 
+    @Test
+    public void testNewInstance() throws Exception {
+        NFeErro erro = new NFeErro();
+        List<NcmBodyResponse> ncms = new ArrayList<>();
+        ncms.add(new NcmBodyResponse());
+
+        NcmsBodyResponse entity = new NcmsBodyResponse();
+        entity.setErro(erro);
+        entity.setNcms(ncms);
+
+        assertNotNull(entity);
+        assertEquals(erro, entity.getErro());
+        assertEquals(ncms, entity.getNcms());
+    }
 }
