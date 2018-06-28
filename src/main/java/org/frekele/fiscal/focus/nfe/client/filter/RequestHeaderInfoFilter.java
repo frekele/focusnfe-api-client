3caf0614f2ebe57b0c3552419f71098fd96dc8b5
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-06-28 03:19:39 -0300

    Http Headers ['X-Api-Cli-Name', 'X-Api-Cli-Version', 'X-Api-Cli-Lang' e 'X-Api-Cli-OS']

--- src/main/java/org/frekele/fiscal/focus/nfe/client/filter/RequestHeaderInfoFilter.java
+++ src/main/java/org/frekele/fiscal/focus/nfe/client/filter/RequestHeaderInfoFilter.java
@@ -4,10 +4,9 @@ import org.frekele.fiscal.focus.nfe.client.core.FocusNFe;
 
 import javax.ws.rs.client.ClientRequestContext;
 import javax.ws.rs.client.ClientRequestFilter;
-import java.io.IOException;
 
 /**
- * Filtro para adicionar Http Headers 'X-Api-Cli-Name' e 'X-Api-Cli-Version' nas Requisições RestEasy.
+ * Filtro para adicionar Http Headers ['X-Api-Cli-Name', 'X-Api-Cli-Version', 'X-Api-Cli-Lang' e 'X-Api-Cli-OS'] nas Requisições RestEasy.
  *
  * @author frekele - Leandro Kersting de Freitas
  */
@@ -15,8 +14,13 @@ import java.io.IOException;
 public class RequestHeaderInfoFilter implements ClientRequestFilter {
 
     @Override
-    public void filter(ClientRequestContext requestContext) throws IOException {
+    public void filter(ClientRequestContext requestContext) {
         requestContext.getHeaders().add("X-Api-Cli-Name", "focusnfe-api-client");
+        //Example: [1.0.2].
         requestContext.getHeaders().add("X-Api-Cli-Version", FocusNFe.class.getPackage().getImplementationVersion());
+        //Example: [Java - 1.8.0_172 - Oracle Corporation].
+        requestContext.getHeaders().add("X-Api-Cli-Lang", "Java - " + System.getProperty("java.version") + " - " + System.getProperty("java.vendor"));
+        //Example: [Windows 10 - amd64].
+        requestContext.getHeaders().add("X-Api-Cli-OS", System.getProperty("os.name") + " - " + System.getProperty("os.arch"));
     }
 }
