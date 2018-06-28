d55867cb357c7165a7b53371b22c09f61ef48977
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2018-06-28 06:53:02 -0300

    FocusMediaType

---
+++ src/main/java/org/frekele/fiscal/focus/nfe/client/core/FocusMediaType.java
@@ -0,0 +1,33 @@
+package org.frekele.fiscal.focus.nfe.client.core;
+
+/**
+ * Interface come media types usados para comunicação com a API.
+ *
+ * @author frekele - Leandro Kersting de Freitas
+ */
+public interface FocusMediaType {
+
+    public static final String APPLICATION_JSON = "application/json";
+
+    public static final String APPLICATION_JSON_CHARSET_UTF8 = APPLICATION_JSON + ";charset=UTF-8";
+
+    public static final String APPLICATION_XML = "application/xml";
+
+    public static final String APPLICATION_XML_CHARSET_UTF8 = APPLICATION_XML + ";charset=UTF-8";
+
+    public static final String APPLICATION_PDF = "application/pdf";
+
+    public static final String APPLICATION_PDF_CHARSET_UTF8 = APPLICATION_PDF + ";charset=UTF-8";
+
+    public static final String APPLICATION_ZIP = "application/zip";
+
+    public static final String APPLICATION_ZIP_CHARSET_UTF8 = APPLICATION_ZIP + ";charset=UTF-8";
+
+    public static final String TEXT_XML = "text/xml";
+
+    public static final String TEXT_XML_CHARSET_UTF8 = TEXT_XML + ";charset=UTF-8";
+
+    public static final String TEXT_HTML = "text/html";
+
+    public static final String TEXT_HTML_CHARSET_UTF8 = TEXT_HTML + ";charset=UTF-8";
+}
